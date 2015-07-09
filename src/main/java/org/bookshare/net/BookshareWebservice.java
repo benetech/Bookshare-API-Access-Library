9ab2a30180df605efbd40e69b3b7588729addfe0
Jake Brownell <jakeb@benetech.org>
2015-07-09 16:07:21 -0500

    minor edits to modifiers/comments

--- src/main/java/org/bookshare/net/BookshareWebservice.java
+++ src/main/java/org/bookshare/net/BookshareWebservice.java
@@ -24,23 +24,23 @@ import org.apache.http.message.BasicHeader;
  * data from Bookshare's Webservice API.
  *
  */
-final public class BookshareWebservice {
+public final class BookshareWebservice {
 
 	// endpoint for bookshare API calls
 	private static String URL = "api.bookshare.org";
 
-    /*
+    /**
      * Default constructor
      */
     public BookshareWebservice() {
-
+        // empty
     }
 
-    /*
+    /**
      * Constructor that allows setting of api host
      * @param apiHost String api host
      */
-    public BookshareWebservice(String apiHost) {
+    public BookshareWebservice(final String apiHost) {
         URL = apiHost;
     }
 
@@ -50,22 +50,21 @@ final public class BookshareWebservice {
 	 * @return MD5 encrypted String.
      * @throws java.io.UnsupportedEncodingException
 	 */
-
-	public String md5sum(String str) throws UnsupportedEncodingException {
+	public String md5sum(final String str) throws UnsupportedEncodingException {
 		byte[] md5sum = null;
 		try{
-			MessageDigest md = MessageDigest.getInstance("MD5");
+			final MessageDigest md = MessageDigest.getInstance("MD5");
 			md5sum = md.digest(str.getBytes("UTF-8"));
 		}
-		catch(NoSuchAlgorithmException e){
+		catch(final NoSuchAlgorithmException e){
 			System.out.println(e);
 		}
 
         return toHex(md5sum);
 	}
 
-    private String toHex(byte[] bytes) {
-        BigInteger bi = new BigInteger(1, bytes);
+    private String toHex(final byte[] bytes) {
+        final BigInteger bi = new BigInteger(1, bytes);
         return String.format("%0" + (bytes.length << 1) + "X", bi);
     }
 
@@ -74,7 +73,7 @@ final public class BookshareWebservice {
 	 * @param inputStream InputStream.
 	 * @return String representation of the InputStream data.
 	 */
-	public String convertStreamToString(InputStream inputStream) {
+	public String convertStreamToString(final InputStream inputStream) {
 		/*
 		 * To convert the InputStream to String we use the BufferedReader.readLine()
 		 * method. We iterate until the BufferedReader return null which means
@@ -82,31 +81,31 @@ final public class BookshareWebservice {
 		 * and returned as String.
 		 */
 		// Construct a BufferedReader for the inputStream
-		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-		StringBuilder sb = new StringBuilder();
+		final StringBuilder sb = new StringBuilder();
 		String line = null;
 		try {
 			// Read each line and append a newline character at the end
 			while ((line = reader.readLine()) != null) {
 				sb.append(line + "\n");
 			}
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				// Close the stream irrespective of whether the read was successful or not
 				inputStream.close();
-			} catch (IOException e) {
+			} catch (final IOException e) {
 				e.printStackTrace();
 			}
 		}
 		// Return the trimmed response String
 		return sb.toString().trim();
 	}
-	
+
 	/**
-	 * Retrieves the InputStream in response to a requested URI. Method also takes care of 
+	 * Retrieves the InputStream in response to a requested URI. Method also takes care of
 	 * user authentication.
 	 * This InputStream needs to be used in appropriate way to handle the response
 	 * depending on the response.
@@ -114,24 +113,24 @@ final public class BookshareWebservice {
 	 * from this InputStream and write to a local file using OutputStream.
 	 * E.g. If the requested URI returns a XML or JSON response, make use of
 	 * corresponding parsers to handle the data.
-	 * 
+	 *
 	 * @param wsPassword password of the Bookshare's web service account.
 	 * @param requestUri The request URI.
 	 * @return InputStream representing the response.
 	 * @throws URISyntaxException
 	 * @throws IOException
 	 */
-	public InputStream getResponseStream(String wsPassword, String requestUri) throws
+	public InputStream getResponseStream(final String wsPassword, final String requestUri) throws
 	URISyntaxException, IOException{
 		HttpEntity entity;
-		
-		HttpResponse response = getHttpResponse(wsPassword, requestUri);
+
+		final HttpResponse response = getHttpResponse(wsPassword, requestUri);
 
 		// Get hold of the response entity
 		entity = response.getEntity();
 		return entity.getContent();
 	}
-	
+
 	/**
 	 * Retrieves an HttpResponse for a requested URI.
 	 * @param wsPassword password of the Bookshare web service account.
@@ -141,34 +140,34 @@ final public class BookshareWebservice {
 	 * @throws IOException
 	 */
 	// Method for retrieving an HttpResponse
-	public HttpResponse getHttpResponse(String wsPassword, String requestUri) throws
+	public HttpResponse getHttpResponse(final String wsPassword, final String requestUri) throws
 	URISyntaxException, IOException{
-		DefaultHttpClient httpclient = new DefaultHttpClient();
+		final DefaultHttpClient httpclient = new DefaultHttpClient();
 
 
 
-		HttpHost targetHost = new HttpHost(URL);
+		final HttpHost targetHost = new HttpHost(URL);
 
 
 
-		URI uri = new URI(requestUri);
+		final URI uri = new URI(requestUri);
 
 		// Prepare a HTTP GET Request
-		HttpGet httpget = new HttpGet(uri);
+		final HttpGet httpget = new HttpGet(uri);
 
 		// Execute the request
 		HttpResponse response;
-		
+
 		if(wsPassword != null){
-			Header header = new BasicHeader("X-password",md5sum(wsPassword));
+			final Header header = new BasicHeader("X-password",md5sum(wsPassword));
 			httpget.setHeader(header);
 		}
-		
+
 		// Get the HttpResponse. Earlier the localcontext was used for the basic authentication.
 		// Now basic authentication is not needed as the developer key is appended to the request
 		//response = httpclient.execute(targetHost, httpget, localcontext);
 		response = httpclient.execute(targetHost, httpget);
-		
+
 		return response;
 	}
 
